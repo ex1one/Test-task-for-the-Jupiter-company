c078b9dccdd39046fac901b12a1a3242d16c696e
ex1one <vyvf11@gmail.com>
2022-06-22 22:17:01 +0500

    Made a burger menu and a close button

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Dispatch, FC, SetStateAction } from 'react';
 import menu from '../../const/menu';
 import styles from './menu.module.scss';
 
-const Menu = () => (
-  <nav className={styles.nav}>
-    <ul>
-      {menu.map((item) => <li key={item.toString()} className={styles.itemMenu}>{item.title}</li>)}
-    </ul>
-  </nav>
-);
+interface IMenuProps {
+  setOpen: Dispatch<SetStateAction<boolean>>
+}
+
+const Menu: FC <IMenuProps> = ({ setOpen }) => {
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.menuContent}>
+        <ul>
+          {menu.map((item) => <li key={item.toString()} className={styles.itemMenu}>{item.title}</li>)}
+        </ul>
+        <div className={styles.close} onClick={handleClick} />
+      </div>
+    </nav>
+  );
+};
 
 export default Menu;
